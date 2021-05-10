2c981c25377b5cbeec10fc60e0cd7d28e2ed2a67
Renato Spakauskas <renatospaka@gmail.com>
2021-05-10 15:53:58 -0300

    ci: :construction: fix token for local container

--- sonar-scanner.sh
+++ sonar-scanner.sh
@@ -2,4 +2,5 @@ sonar-scanner \
   -Dsonar.projectKey=go-math \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=5b63323a71c3a2b4a9d93eb46b1d7d072e74877a
+  -Dsonar.login=c0b169290062fcbd44d0e0a94e5c87257936f28c
+  
